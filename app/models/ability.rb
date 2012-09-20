adc5c172ea70aa33e5a4d1ab86ea3262b79433c0
NILID <dm.ilin@mail.ru>
2012-09-20 17:13:55 +0400

    Add cancan rules and change role for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,14 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
+    user ||= User.new # guest user (not logged in)
+    if user.role? :admin
+       can :manage, :all
+    elsif user.role? :user
+       can :read, :all
+    else
+       can :read, :all
+    end
     #   user ||= User.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,41 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
+  after_create :init_user
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :login, :roles
   # attr_accessible :title, :body
 
-  has_one :user
+  extend FriendlyId
+  friendly_id :login, use: :slugged
 
+  validates :login, :presence => true, :uniqueness => true
+  validates_format_of :login, :with => /\A[A-Za-z0-9_]+\z/
+  validates_length_of :login, :maximum => 9
+
+  has_one :setting
+
+  ROLES = %w[admin user moderator]
+
+  def roles=(roles)
+      self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+  end
+
+  def roles
+      ROLES.reject { |r| ((roles_mask || 0) & 2**ROLES.index(r)).zero? }
+  end
+
+  def role?(role)
+      roles.include? role.to_s
+  end
+
+  private
+    def init_user
+      self.build_setting
+    end
 end
