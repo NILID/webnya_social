1f358e5372cf49fc7f59f50118ffe6027a7ecf0e
NILID <dm.ilin@mail.ru>
2012-09-20 17:12:35 +0400

    Add friendly_id slug and login

--- db/migrate/20120920081244_devise_create_users.rb
+++ db/migrate/20120920081244_devise_create_users.rb
@@ -1,6 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+
+      t.string :login,              :null => false
+      t.string :slug
+
+      t.integer :roles_mask,        :default => "2"
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -37,6 +42,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :users, :login,                :unique => true
+    add_index :users, :slug,                 :unique => true
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     add_index :users, :confirmation_token,   :unique => true
