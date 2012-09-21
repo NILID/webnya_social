660276b0a70c5c8d5923724a0293060a20dfdc2a
NILID <dm.ilin@mail.ru>
2012-09-21 14:52:25 +0400

    add validation for bg

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,7 +1,11 @@
 class Setting < ActiveRecord::Base
   belongs_to :user
   attr_accessible :bg
+  validates_attachment :bg, :content_type => { :content_type => [ "image/jpg", "image/jpeg", "image/gif", "image/png", "image/pjpeg", "image/x-png" ] },
+                                 :size => { :in => 0..5.megabytes }
 
   has_attached_file :bg, :styles => { :large => "1024x768#", :thumb => "100x100#" }
+
+
 
 end
