e248db628a398f3c878eb298741e4e80d615f05b
NILID <dm.ilin@mail.ru>
2012-09-21 11:11:01 +0400

    some modify for settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -15,35 +15,17 @@ class SettingsController < ApplicationController
   end
 
   def create
-    respond_to do |format|
-      if @setting.save
-        format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
-        format.json { render json: @setting, status: :created, location: @setting }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @setting.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
     respond_to do |format|
       if @setting.update_attributes(params[:setting])
-        format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
+        format.html { redirect_to user_setting_path(@user), notice: 'Setting was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
         format.json { render json: @setting.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  def destroy
-    @setting.destroy
-
-    respond_to do |format|
-      format.html { redirect_to settings_url }
-      format.json { head :no_content }
     end
   end
 end
