b84e19f3e56cb011abbf34509819489d1c20a2ac
NILID <dm.ilin@mail.ru>
2012-09-24 12:54:05 +0400

    Add convert option for large style

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,7 +4,7 @@ class Setting < ActiveRecord::Base
   validates_attachment :bg, :content_type => { :content_type => [ "image/jpg", "image/jpeg", "image/gif", "image/png", "image/pjpeg", "image/x-png" ] },
                                  :size => { :in => 0..5.megabytes }
 
-  has_attached_file :bg, :styles => { :large => "1024x768#", :thumb => "100x100#" }
+  has_attached_file :bg, :styles => { :large => "1024x768#", :thumb => "100x100#"}, :convert_options => {:large => "-quality 65"}
 
 
 
