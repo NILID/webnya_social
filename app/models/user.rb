bda6ef27fe2da93fe9fc4d4342c1e794c1747b25
NILID <dm.ilin@mail.ru>
2013-02-25 16:25:46 +0400

    add current init_setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  has_one :setting
 
-  after_create :init_user
+  after_create :init_setting
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
@@ -18,7 +19,7 @@ class User < ActiveRecord::Base
   validates_format_of :login, :with => /\A[A-Za-z0-9_]+\z/
   validates_length_of :login, :maximum => 9
 
-  has_one :setting
+
 
   ROLES = %w[admin user moderator]
 
@@ -35,7 +36,8 @@ class User < ActiveRecord::Base
   end
 
   private
-    def init_user
-      self.build_setting
-    end
+  def init_setting
+    setting = self.build_setting
+    setting.save!
+  end
 end
